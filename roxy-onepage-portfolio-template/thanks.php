65d97cdbe57dbc07b8eeaee4f9f16960201e5da5
Your Name <you@example.com>
2020-03-16 12:34:56 +0600

    first commit

---
+++ roxy-onepage-portfolio-template/thanks.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Rumu - Freelancer One Page Portfolio & Resume HTML5 Template">
+		<meta name="keywords" content="resume, cv, portfolio, personal, developer, designer,personal resume, onepage, clean, modern">
+		<meta name="author" content="Nababur">
+		<!-- Page Title -->
+		<title>Rumu | One Page Personal Portfolio HTML5 Template</title>
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+		<!-- Google Fonts css-->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700" rel="stylesheet"> 
+		<!--Reset normalize css -->
+        <link rel="stylesheet" href="assets/css/normalize.css">
+		<!--Reset main css -->
+        <link rel="stylesheet" href="assets/css/main.css">
+		<!-- Bootstrap css -->
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<!-- Animate css -->
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<!-- Owlcarousel css -->
+		<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+		<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+		<!-- Font Awesome icon css-->
+		<link href="assets/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" media="screen">
+		<!-- Custom css -->
+		<link href="assets/css/custom.css" rel="stylesheet" media="screen">
+	    <!-- Responsive css -->
+	    <link rel="stylesheet" href="assets/css/responsive.css">
+
+		<!--Modernizr Js-->
+        <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+		
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+    </head>
+    <body>
+		
+		<!-- Thanks message Section -->
+		<footer class="">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="thanks_message text-center">
+							<h1>Your message has been sent !</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!--Thanks message Section -->
+		
+		<!--All Jquery Library Files -->
+		<script src="assets/js/jquery-min.js"></script>
+		<!-- Bootstrap js  -->
+        <script src="assets/js/bootstrap.min.js"></script>
+		<!-- isotope js  -->
+		<script src="assets/js/isotope.min.js"></script>
+		<!-- particles js  -->
+		<script src="assets/js/particles.js"></script>
+		<script src="assets/js/app.js"></script>
+		<!-- Typed js  -->
+		<script src="assets/js/typed.js"></script>
+		<!-- Parallax js  -->
+		<script src="assets/js/jquery.parallax-1.1.3.js"></script>		
+		<!-- Wow Min js  -->
+		<script src="assets/js/wow.min.js"></script>
+		<!-- Counter Up js  -->
+		<script src="assets/js/waypoints.min.js"></script>
+		<script src="assets/js/jquery.counterup.min.js"></script>
+		<!-- Owl Carousel  js  -->
+		<script src="assets/js/owl.carousel.min.js"></script>
+		<!-- scripts Js -->
+        <script src="assets/js/scripts.js"></script>
+
+    </body>
+</html>
